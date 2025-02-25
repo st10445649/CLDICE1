a04c3ae1037f14e3aafa18495780379cf487f2cd
Zahraa Goga <st10445649@vcconnect.edu.za>
2025-02-25 12:32:59 +0200

    add search commit

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,10 +20,23 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Movie.ToListAsync());
-        }
+       public async Task<IActionResult> Index(string searchString)
+{
+    if (_context.Movie == null)
+    {
+        return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+    }
+
+    var movies = from m in _context.Movie
+                select m;
+
+    if (!String.IsNullOrEmpty(searchString))
+    {
+        movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+    }
+
+    return View(await movies.ToListAsync());
+}
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d04471600a6a0f22872e348e25140ff08a2dc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e38ff78f2c3217542e669ff10ee49dc34155a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
