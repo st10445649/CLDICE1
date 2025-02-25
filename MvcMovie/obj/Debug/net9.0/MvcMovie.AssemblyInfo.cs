b8d60fc6796a36a20f79c0de3c16dce8cf777dca
Zahraa Goga <st10445649@vcconnect.edu.za>
2025-02-25 13:06:23 +0200

    Search feature 2

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e38ff78f2c3217542e669ff10ee49dc34155a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04c3ae1037f14e3aafa18495780379cf487f2cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
